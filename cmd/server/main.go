0556d3058361c2ab5ea6d80ec9e94c860b1f4ee9
Amir Khashimov <amir.khashimov@mostgames.org>
2026-09-14 01:08:53 +0300

    step 5. get messages + added logs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,6 @@
 package main
 
 import (
-	"container/list"
 	"encoding/json"
 	"io"
 	"log"
@@ -16,8 +15,12 @@ import (
 	"time"
 )
 
-var messages = list.New()
-var processMessage = getMessageProceessor()
+// List I used before cannot be simply serialized as complex structure, so I decided to proceed with slices and simple reverse
+var (
+	messages       []Message
+	messagesMu     sync.Mutex
+	processMessage = getMessageProceessor()
+)
 
 type Message struct {
 	Id        int    `json:"id"`
@@ -39,6 +42,7 @@ func main() {
 	mux.HandleFunc("GET /health", checkHealth)
 	mux.HandleFunc("POST /echo", echoMessage)
 	mux.HandleFunc("POST /messages", sendMessage)
+	mux.HandleFunc("GET /messages", getMessages)
 
 	// TODO Этап 1: GET /health           -> 200, тело "ok"
 	// TODO Этап 2: POST /echo            -> тело запроса без изменений
@@ -52,14 +56,21 @@ func main() {
 }
 
 func checkHealth(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[GET /health] Health check started")
+	defer log.Printf("[GET /health] Health check completed")
+
 	w.Write([]byte("ok"))
 }
 
 func echoMessage(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[POST /echo] Echoing message started")
+	defer log.Printf("[POST /echo] Echoing message completed")
+
 	defer r.Body.Close()
 
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
+		log.Printf("[POST /echo] Failed to load the request body. Return 500")
 		http.Error(w, "Failed to load the request body", http.StatusInternalServerError)
 		return
 	}
@@ -68,20 +79,25 @@ func echoMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", contentType)
 
 	if contentType == "application/json" && !json.Valid(bodyBytes) {
-		http.Error(w, "Content is not valid", http.StatusBadRequest)
+		log.Printf("[POST /echo] Request content is not valid. Return 400")
+		http.Error(w, "Request content is not valid", http.StatusBadRequest)
 		return
 	} else {
+		log.Printf("[POST /echo] Successfully read request body: \"%v\". Return 200", string(bodyBytes))
 		w.WriteHeader(http.StatusOK)
 		w.Write(bodyBytes)
 	}
 }
 
 func sendMessage(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[POST /messages] Sending message started")
+	defer log.Printf("[POST /messages] Sending message completed")
 
 	var data map[string]string
 
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		log.Printf("[POST /messages] Request content is not valid. Return 400")
+		http.Error(w, "Request content is not valid", http.StatusBadRequest)
 		return
 	}
 
@@ -89,6 +105,7 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 
 	requestMessage, exists := data["message"]
 	if !exists || requestMessage == "" {
+		log.Printf("[POST /messages] 'message' should not be empty. Return 400")
 		http.Error(w, "'message' should not be empty", http.StatusBadRequest)
 		return
 	}
@@ -96,28 +113,72 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	responseMessage, err := processMessage(requestMessage)
+
 	if err != nil {
+		log.Printf("[POST /messages] Cannot process message: %v. Return 500", err.Error())
 		http.Error(w, "Cannot process message: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	log.Printf("[POST /messages] Message processed successully: \"%v\". Return 201", responseMessage)
 	w.WriteHeader(http.StatusCreated)
 
 	if err := json.NewEncoder(w).Encode(responseMessage); err != nil {
+		log.Printf("[POST /messages] Cannot serialize message: %v. Return 500", err.Error())
 		http.Error(w, "Cannot serialize message: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
+func getMessages(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[GET /messages] Getting messages started")
+	defer log.Printf("[GET /messages] Getting messages completed")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(getMessagesReverse()); err != nil {
+		log.Printf("[GET /messages] Cannot serialize message: %v. Return 500", err.Error())
+		http.Error(w, "Cannot serialize message: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer log.Printf("[GET /messages] Successfully serialized messages. Messages count: %v", len(messages))
+}
+
+func getMessagesReverse() []Message {
+	messagesMu.Lock()
+	defer messagesMu.Unlock()
+
+	log.Printf("[getMessagesReverse] Message processing started")
+	defer log.Printf("[getMessagesReverse] Message processing completed")
+
+	n := len(messages)
+	if n == 0 {
+		return make([]Message, 0)
+	}
+
+	result := make([]Message, n)
+
+	for i, msg := range messages {
+		result[n-1-i] = msg
+	}
+
+	return result
+}
+
 func getMessageProceessor() func(string) (Message, error) {
 	currentId := 0
-	var mu sync.Mutex
+	log.Printf("[getMessageProcessor] Message processor initialized, ID = %v", currentId)
 
 	return func(messageText string) (Message, error) {
-		mu.Lock()
-		defer mu.Unlock()
+		messagesMu.Lock()
+		defer messagesMu.Unlock()
 
 		currentId++
+
+		log.Printf("[getMessageProcessor] Message processing started, ID = %v", currentId)
+		defer log.Printf("[getMessageProcessor] Message processing completed")
+
 		now := time.Now().UTC()
 		formattedTime := now.Format(time.RFC3339)
 		message := Message{
@@ -126,7 +187,7 @@ func getMessageProceessor() func(string) (Message, error) {
 			CreatedAt: formattedTime,
 		}
 
-		messages.PushFront(message)
+		messages = append(messages, message)
 
 		return message, nil
 	}
